8fd62aa51ff2471552538ab4d193b16f7906e56e
Zhayida Haishan <130613310+ZayidaQ@users.noreply.github.com>
2023-06-23 13:07:00 -0700

    update

--- scripts/posts.js
+++ scripts/posts.js
@@ -9,3 +9,22 @@ btnLogOut.addEventListener("click", function(event) {
     event.preventDefault;
     logout();
 })
+
+
+// this code is for the side menu to work properly
+const profileMenu = document.getElementById("profileMenu");
+const sideActivity = document.getElementById("sidebarActivity");
+const moreLink = document.getElementById("showMoreLink");
+
+function toggleMenu() {
+  profileMenu.classList.toggle("open-menu");
+}
+
+function toggleActivity() {
+  sideActivity.classList.toggle("open-activity");
+
+  moreLink.innerHTML = sideActivity.classList.contains("open-activity") ? "Show less <b>-</b>" : "Show more <b>+</b>";
+}
+
+profileMenu.addEventListener("click", toggleMenu);
+moreLink.addEventListener("click", toggleActivity);
