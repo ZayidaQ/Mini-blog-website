ff3ca3fbbd5a4fb580f03f5938c27e5361955c70
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-06-23 12:00:09 -0700

    Added logout button functionality
    Added logout button functionality

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,3 +1,11 @@
 /* Posts Page JavaScript */
+// import {logout} from "./auth.js"
 
 "use strict";
+
+const btnLogOut = document.querySelector("#btnLogOut");
+
+btnLogOut.addEventListener("click", function(event) {
+    event.preventDefault;
+    logout();
+})
