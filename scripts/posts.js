36829c6963066821274bbd593566d3943d2590e3
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-06-24 02:09:29 -0700

    Added display all posts functionality

--- scripts/posts.js
+++ scripts/posts.js
@@ -2,12 +2,74 @@
 
 "use strict";
 
+// global variables
 const btnLogOut = document.querySelector("#btnLogOut");
+// const baseURL = "https://microbloglite.herokuapp.com/";
+const formCreatePost = document.querySelector("#formCreatePost");
+const bearerToken = getLoginData();
+const displayPosts = document.querySelector("#displayPosts");
 
-btnLogOut.addEventListener("click", function(event) {
+// when page loads
+window.onload = () => {
+  displayAllPosts();
+}
+
+// logout button event listener
+btnLogOut.onclick = function(event) {
     event.preventDefault;
     logout();
-})
+}
+
+// when button is clicked, create a post using value from form
+formCreatePost.onsubmit = function(event) {
+  event.preventDefault;
+  createPost();
+}
+
+// function for creating a new post
+async function createPost() {
+  const newPost = document.querySelector("#textBoxPost");
+  try {
+    const response = await fetch(`${apiBaseURL}/api/posts`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        "text": newPost.value
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "Authorization": "Bearer " + bearerToken.token},
+    });
+    const data = await response.json();
+    console.log(data);  //test
+  }
+  catch(error) {
+    console.log(error); //test
+  }
+}
+
+// function for displaying all posts
+async function displayAllPosts() {
+  let display = "";
+  try{
+    const response = await fetch(`${apiBaseURL}/api/posts`,
+    {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer " + bearerToken.token},
+    });
+    const data = await response.json();
+    let newData = Object.values(data);
+    newData.forEach(post => {
+      displayPosts.innerHTML += `<p>${post.text}</p>`
+    }) 
+    console.log(Object.values(data));
+  }
+  catch(error) {
+    console.log(error); //test
+    displayPosts.innerHTML = error;
+  }
+}
 
 
 // this code is for the side menu to work properly
