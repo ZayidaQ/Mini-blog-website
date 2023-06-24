721195d2fb0b64415f85004d763b583ccb052391
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-06-24 14:46:46 -0700

    Fixed comments
    Fixed comments

--- scripts/posts.js
+++ scripts/posts.js
@@ -83,19 +83,19 @@ async function displayAllPosts() {
 
 
 // this code is for the side menu to work properly
-// const profileMenu = document.getElementById("profileMenu");
-// const sideActivity = document.getElementById("sidebarActivity");
-// const moreLink = document.getElementById("showMoreLink");
+const profileMenu = document.getElementById("profileMenu");
+const sideActivity = document.getElementById("sidebarActivity");
+const moreLink = document.getElementById("showMoreLink");
 
-// function toggleMenu() {
-//   profileMenu.classList.toggle("open-menu");
-// }
+function toggleMenu() {
+  profileMenu.classList.toggle("open-menu");
+}
 
-// function toggleActivity() {
-//   sideActivity.classList.toggle("open-activity");
+function toggleActivity() {
+  sideActivity.classList.toggle("open-activity");
 
-//   moreLink.innerHTML = sideActivity.classList.contains("open-activity") ? "Show less <b>-</b>" : "Show more <b>+</b>";
-// }
+  moreLink.innerHTML = sideActivity.classList.contains("open-activity") ? "Show less <b>-</b>" : "Show more <b>+</b>";
+}
 
-// profileMenu.addEventListener("click", toggleMenu);
-// moreLink.addEventListener("click", toggleActivity);
+profileMenu.addEventListener("click", toggleMenu);
+moreLink.addEventListener("click", toggleActivity);
