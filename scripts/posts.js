8195a1d3cac64ea88f894ec687765492b7309c27
Zhayida Haishan <130613310+ZayidaQ@users.noreply.github.com>
2023-06-26 11:16:25 -0700

    update

--- scripts/posts.js
+++ scripts/posts.js
@@ -111,20 +111,9 @@ function displayAllPosts(_username, _date, _text, _numLikes, _ownPost) {
       `
 }
 
-// this code is for the side menu to work properly
-const profileMenu = document.getElementById("profileMenu");
-const sideActivity = document.getElementById("sidebarActivity");
-const moreLink = document.getElementById("showMoreLink");
+// THis is for toggle
+const settingsMenu = document.querySelector(".settings-menu");
 
-function toggleMenu() {
-  profileMenu.classList.toggle("open-menu");
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
 }
-
-function toggleActivity() {
-  sideActivity.classList.toggle("open-activity");
-
-  moreLink.innerHTML = sideActivity.classList.contains("open-activity") ? "Show less <b>-</b>" : "Show more <b>+</b>";
-}
-
-profileMenu.addEventListener("click", toggleMenu);
-moreLink.addEventListener("click", toggleActivity);
---
+++ scripts/test.js
@@ -0,0 +1,5 @@
+const settingsMenu = document.querySelector(".settings-menu");
+
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
+}
