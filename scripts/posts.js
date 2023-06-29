32a41aa23e1826415e7fbbd5e3e0a8167b1582ab
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-06-28 20:07:38 -0700

    Loading screen in posts page
    Loading screen in posts page

--- scripts/posts.js
+++ scripts/posts.js
@@ -21,6 +21,24 @@ window.onload = () => {
   dropdownSortPosts.onchange = onDropdownSort;
 }
 
+const wait = (delay = 0) =>
+new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (elementOrSelector, visible) => 
+(typeof elementOrSelector === 'string'
+    ? document.querySelector(elementOrSelector)
+    : elementOrSelector
+).style.display = visible ? 'block' : 'none';
+
+setVisible('.page', false);
+setVisible('#loading', true);
+
+document.addEventListener('DOMContentLoaded', () =>
+wait(1000).then(() => {
+    setVisible('.page', true);
+    setVisible('#loading', false);
+}));
+
 // Show or hide button that scrolls to the top of page
 window.onscroll = () => {
   scrollFunction();
@@ -194,7 +212,6 @@ function displayAllPosts(_username, _date, _text, _numLikes, _ownPost, _valueID,
 
                 <div class="post-stats">
                     <div>
-                        <i class="fas fa-heart" style="color: #ef4f4f;"></i>
                         <span class="liked-users"> ${_numLikes} likes </span>
                     </div>
                     <div>
